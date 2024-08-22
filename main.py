ade54ef46d8c2a2dc677d783552419af1dc218e7
Marcin Bogusz <bogusz.marcin@icloud.com>
2024-08-22 07:14:41 +0200

    Added draw spirograph function.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import turtle as t
 import random
 
-tim = t.Turtle("turtle")
+# Assignments for random walk
+jim = t.Turtle("turtle")
 t.colormode(255)
-tim.pensize(5)
-tim.speed("fast")
+jim.pensize(5)
+jim.speed("fast")
 directions = [0, 90, 180, 270]
 
 
@@ -19,13 +20,27 @@ def random_color():
 
 def random_walk(color_set, directions_set, distance):
     """Return random walk"""
-    tim.color(color_set)
-    tim.forward(distance)
-    tim.setheading(random.choice(directions_set))
+    jim.color(color_set)
+    jim.forward(distance)
+    jim.setheading(random.choice(directions_set))
 
 
+# Draw a random wolk with random color
 for i in range(200):
     random_walk(random_color(), directions, 30)
+
+tim = t.Turtle("circle")
+tim.speed("fast")
+
+
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        jim.color(random_color())
+        jim.circle(10)
+        jim.setheading(tim.heading() + size_of_gap)
+
+
+# draw_spirograph(25)
 
 screen = t.Screen()
 screen.exitonclick()
